3254490ebb27d572f8583bed3d0c8255300fbacb
김재승 <jaes2878@gmail.com>
2025-10-15 16:31:19 +0900

    feat : 하단 비디오 배경 이미지 크기 조절

--- HanwhaIgilShootClient/src/components/BottomVideo.tsx
+++ HanwhaIgilShootClient/src/components/BottomVideo.tsx
@@ -1,6 +1,6 @@
 function BottomVideo() {
   return (
-    <div className="bg-[url('./img/baseballBack.svg')] bg-center bg-no-repeat flex flex-col items-center w-full mt-80 relative p-10">
+    <div className="bg-[url('./img/baseballBack.svg')] bg-cover  bg-center bg-no-repeat flex flex-col items-center w-full mt-80 relative p-10">
       <p className="text-5xl font-bold absolute top-[7px] gradient-flip-text">
         NO FEAR, JUST FLIGHT
       </p>
